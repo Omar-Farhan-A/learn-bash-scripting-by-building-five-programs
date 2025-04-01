2cad32c772168240df4499e4814d8535bb9f0c23
Omar Farhan <omarfarhan516@gmail.com>
2025-04-01 09:01:33 +0000

    Save progress: 730.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-echo -e "\n~~ Questionnaire ~~\n"
-QUESTION1="What's your name?"
-echo $QUESTION1
-read NAME
-QUESTION2="Where are you from?"
-echo $QUESTION2
-read LOCATION
-echo Hello $NAME from $LOCATION.
-QUESTION3="What's your favorite coding website?"
-echo $QUESTION3
-read WEBSITE
-echo $WEBSITE
+#Program that counts down to zero from a given argument
+echo -e "\n~~ Countdown Timer ~~\n"
+: '
+for (( i = $1; i >= 0; i-- ))
+do
+  echo $i
+  sleep 1
+done
+'
+I=$1
+
+
+echo $I
